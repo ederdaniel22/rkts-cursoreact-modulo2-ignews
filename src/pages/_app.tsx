6e392dd070092a59de856bfaef91f93b717c2295
EDER DANIEL DOMINGUES <60990935+ederdaniel22@users.noreply.github.com>
2022-05-31 17:21:09 -0400

    Finalização API routes Next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
+import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header';
+
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthSessionProvider session={pageProps.session}>
+
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthSessionProvider>
   )
 }
 
